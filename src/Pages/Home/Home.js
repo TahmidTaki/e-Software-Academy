de76b9ff5d5cdfb858609b8c880413499c6b9ac8
TahmidTaki <tahmid.taki@live.iium.edu.my>
2022-10-25 14:13:54 +0800

    navbar ar footer implemented

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,29 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h3>Home Component</h3>
+      <div
+        className="hero min-h-screen"
+        style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}
+      >
+        <div className="hero-overlay bg-opacity-75"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md bg-stone-500 p-10 rounded-md">
+            <h1 className="mb-5 text-5xl font-bold">
+              Become A Software Engineer With Us
+            </h1>
+            <p className="mb-5 text-lg font-semibold">
+              Software engineers, often referred to as software developers,
+              design innovative software for businesses and clients. From a
+              personal banking app to a company’s project management workflow
+              system, software engineers are behind many of the essential apps
+              and programs that make business and daily life more efficient.
+              Interested in changing your career to software
+              development/engineering?
+            </p>
+            <button className="btn btn-accent">Register</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,64 @@
 import React from "react";
+import { MdCastForEducation } from "react-icons/md";
 
 const Header = () => {
   return (
-    <div>
-      <h3>Header section.................</h3>
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Courses</a>
+            </li>
+            <li>
+              <a>FAQ</a>
+            </li>
+
+            <li>
+              <a>Blog</a>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost normal-case text-xl">
+          <MdCastForEducation className="mx-1" /> 𝚎-𝚂𝚘𝚏𝚝𝚠𝚊𝚛𝚎-𝙰𝚌𝚊𝚍𝚎𝚖𝚢
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">
+          <li>
+            <a>Courses</a>
+          </li>
+          <li>
+            <a>FAQ</a>
+          </li>
+
+          <li>
+            <a>Blog</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Log In</a>
+      </div>
     </div>
   );
 };
